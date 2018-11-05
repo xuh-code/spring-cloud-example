2fb282366462d1e6e11f83efcd04b470ed883db9
xuh-code <254490061@qq.com>
2018-11-06 00:58:23 +0800

    创建vue项目

---
+++ com.evntech.jiabo.vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main/java/com/evntech/jiabo/EvntechJiaboApplication.java
@@ -0,0 +1,12 @@
+package com.evntech.jiabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvntechJiaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvntechJiaboApplication.class, args);
+	}
+}
