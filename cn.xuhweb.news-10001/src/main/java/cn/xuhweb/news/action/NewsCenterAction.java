6ec0ba0b0f8a3ae6fe01238098787247bd5a9142
xuh-code <254490061@qq.com>
2018-12-11 02:18:37 +0800

    添加注册中心7001 / 7002 添加新闻中心

---
+++ cn.xuhweb.news-10001/src/main/java/cn/xuhweb/news/action/NewsCenterAction.java
@@ -0,0 +1,25 @@
+package cn.xuhweb.news.action;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xuh
+ * DATE 2018/12/11 1:11.
+ * version 1.0
+ */
+@RestController
+@RequestMapping("/news")
+public class NewsCenterAction {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/get")
+    public String home(@RequestParam(value = "name") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+}
